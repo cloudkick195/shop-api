eae75bbe3b2e24f0b194a3cb3ca5ce86ce4c66db
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2021-05-01 17:03:51 +0700

    update

--- source/src/helpers/kiotvietApi.helper.ts
+++ source/src/helpers/kiotvietApi.helper.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "../decorators/injectable.decorator";
 import axios from 'axios';
+import qs from 'qs'
 @Injectable()
 export class kiotvietApi {
   async GetProductKiotviet(code: string): Promise<any> {
@@ -11,6 +12,27 @@ export class kiotvietApi {
     })
     
     return product
+  }
+
+  async getAccessTokenKiotviet() : Promise<any> {
+    try {
+      const data: any = {
+          "client_id": process.env.CLIENT_ID,
+          "client_secret": process.env.CLIENT_SECRET,
+          "grant_type": process.env.GRANT_TYPE,
+          "scopes": process.env.SCOPES,
+      }
+  
+      const res: any = await axios.post(process.env.KIOTVIET_URL_TOKEN, qs.stringify(data), {
+          headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+          }
+      })
+
+      return res.data.access_token
+  } catch (error) {
+      return error
+  }
   }
 
   constructor() {}
